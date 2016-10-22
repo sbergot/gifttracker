8feaabd8590e39748f29ba60e9decc92117e8efe
Simon Bergot <simon.bergot@gmail.com>
2016-10-22 10:29:10 +0200

    add webpack.config and remove useless js files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+
+module.exports = {
+    externals : {
+        "jquery" : "jQuery"
+    },
+    entry: './typescript/gift.ts',
+    output: {
+        filename: 'wwwroot/js/gift.js'
+    },
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}

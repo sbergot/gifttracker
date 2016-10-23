e43c6d6c16bf58f7c816dcd0c8a29901de5df259
Simon Bergot <simon.bergot@gmail.com>
2016-10-23 09:19:11 +0200

    gift edition is working on client

--- typescript/gift.ts
+++ typescript/gift.ts
@@ -3,15 +3,21 @@ import * as lodash from "lodash"
 import "bootstrap"
 
 interface Gift {
-    id : Number;
-    applicationUserId : Number;
+    id : number;
+    applicationUserId : number;
     title : string;
     description : string;
 }
 
 interface State {
-    gifts : Gift[]
+    gifts : {[index : number] : Gift};
+    currentEdit : number | null;
 }
+
+let state : State = {
+    gifts : [],
+    currentEdit : null
+};
 
 let gifturl = "./api/gift";
 let giftIdAttr = "gift-id";
@@ -58,23 +64,45 @@ function viewGift(gift : Gift) : string {
 // function
 
 function renderState(state : State) {
-    let giftsHtml = state.gifts.map(viewGift).join("");
-    jquery("#gift-list").append(giftsHtml);
+    let giftsHtml = lodash.values(state.gifts).map(viewGift).join("");
+    jquery("#gift-list").html(giftsHtml);
     jquery(".gift-view").map((i, e) => {
         let view = $(e);
-        let id = view.attr(giftIdAttr);
-        view.find("button").click(() => { jquery("#gift-edit").modal(); });
+        let id = parseInt(view.attr(giftIdAttr));
+        view.find("button").click(() => { editGift(id) });
     });
 }
 
-let state : State = {
-    gifts : []
-};
+function mountModal(state : State)
+{
+    jquery("#gift-edit-save").on("click", () => {
+        let currentEdit = state.currentEdit;
+        if (currentEdit === null) {
+            console.error("cannot save because current edit gift id is null");
+            return;
+        }
+        let currentGift = state.gifts[currentEdit];
+        currentGift.title = jquery("#gift-edit-title").val();
+        currentGift.description = jquery("#gift-edit-description").val();
+        jquery("#gift-edit").modal("toggle");
+        renderState(state);
+    });
+}
+
+function editGift(giftId : number) {
+    state.currentEdit = giftId;
+    let gift = state.gifts[giftId];
+    jquery("#gift-edit-title").val(gift.title);
+    jquery("#gift-edit-description").val(gift.description);
+    jquery("#gift-edit").modal();
+}
 
 getGifts().then((response) => {
     if (response === undefined) {
         return;
     }
-    state.gifts = response;
+    let gifts = response as Gift[];
+    state.gifts = lodash.keyBy(gifts, (g) => g.id);
     renderState(state);
+    mountModal(state);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 var webpack = require('webpack');
+var path = require('path');
+
+let tsPath = path.resolve("./typescript");
 
 module.exports = {
     externals : {
         "jquery" : "jQuery",
-        "bootstrap" : "jQuery"
+        "bootstrap" : "jQuery",
+        "lodash" : "_"
     },
     entry: './typescript/gift.ts',
     output: {
         filename: 'wwwroot/js/gift.js'
     },
     resolve: {
-      extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+        root : tsPath,
+        extensions: ['', '.ts']
     },
     module: {
         loaders: [
-            { test: /\.ts$/, loader: 'ts-loader' }
+            { test: /\.ts$/, loader: 'ts-loader', include: [ tsPath ] }
         ]
     }
 }

136bf12922e09e458c50e51c55df99e30c3003ee
Simon Bergot <sbergot@talentsoft.com>
2016-12-07 20:26:57 +0100

    vue now working

--- typescript/app.vue
+++ typescript/app.vue
@@ -1,14 +1,19 @@
 <template>
-<h1 @click="hello">hello world</h1>
+<h1 @click="hello">hello {{name}}</h1>
 </template>
 
 <script>
-export default {
-  methods: {
-    // type annotation!
-    hello(): void {
-      alert('hello world')
-    }
+// import dependency
+import {Vue, Component} from 'av-ts'
+
+// decorat vue class
+@Component
+export default class App extends Vue {
+  // undecorated property will be packed in `data` option
+  name = 'world'
+  // undecorated method is just method
+  hello() {
+    alert('hello ' + this.name)
   }
 }
 </script>

0d8e6ebeb68726177f96724805cdd76239ad7398
Simon Bergot <sbergot@talentsoft.com>
2016-12-11 10:00:18 +0100

    update individual model

--- Models/Individual.cs
+++ Models/Individual.cs
@@ -4,6 +4,8 @@ namespace WebApplication.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }

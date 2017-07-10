af06df3591168f24ea06b1e35e96e8641617ab4a
Simon Bergot <sbergot@talentsoft.com>
2017-07-10 07:46:45 +0200

    enrich init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 INSERT INTO public."Individuals"(
 	"FirstName", "LastName", "BirthDay")
-	VALUES ('John', 'Smith', '1985-05-24');
+	VALUES ('Michael', 'Keaton', '1984-06-18');
 
 INSERT INTO public."Individuals"(
 	"FirstName", "LastName", "BirthDay")
@@ -14,6 +14,14 @@ INSERT INTO public."Events"(
 	"Type", "Year")
 	VALUES (1, 2016);
 
+INSERT INTO public."Events"(
+	"Type", "Year")
+	VALUES (0, 2017);
+
+INSERT INTO public."Events"(
+	"Type", "Year")
+	VALUES (1, 2017);
+
 INSERT INTO public."Gifts"(
 	"Title",
 	"Description",
@@ -25,7 +33,42 @@ SELECT
 	'test 1 ',
 	'bla bla bla',
 	58,
-	"Id",
+	"AspNetUsers"."Id",
 	2,
 	1
 FROM "AspNetUsers" WHERE "Email" = 'simon.bergot@gmail.com'
+FROM "Individuals" WHERE "Name" = 'Smith'
+
+INSERT INTO public."Gifts"(
+	"Title",
+	"Description",
+	"PriceInCents",
+	"OwnerId",
+	"ReceiverId",
+	"EventId")
+SELECT
+	'test 1 ',
+	'bla bla bla',
+	58,
+	"AspNetUsers"."Id",
+	2,
+	1
+FROM "AspNetUsers" WHERE "Email" = 'simon.bergot@gmail.com'
+FROM "Individuals" WHERE "Name" = 'Smith'
+
+INSERT INTO public."Gifts"(
+	"Title",
+	"Description",
+	"PriceInCents",
+	"OwnerId",
+	"ReceiverId",
+	"EventId")
+SELECT
+	'test 1 ',
+	'bla bla bla',
+	58,
+	"AspNetUsers"."Id",
+	2,
+	1
+FROM "AspNetUsers" WHERE "Email" = 'simon.bergot@gmail.com'
+FROM "Individuals" WHERE "Name" = 'Smith'

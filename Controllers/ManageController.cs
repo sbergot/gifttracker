60694db103c32a67a044c87dff0ebec19f73609f
Simon Bergot <sbergot@talentsoft.com>
2017-08-19 11:04:29 +0200

    finish dotnet 2 migration

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -264,7 +264,8 @@ namespace WebApplication.Controllers
                 return View("Error");
             }
             var userLogins = await _userManager.GetLoginsAsync(user);
-            var otherLogins = _signInManager.GetExternalAuthenticationSchemes().Where(auth => userLogins.All(ul => auth.AuthenticationScheme != ul.LoginProvider)).ToList();
+            var authSchemes = await _signInManager.GetExternalAuthenticationSchemesAsync();
+            var otherLogins = authSchemes.Where(auth => userLogins.All(ul => auth.Name != ul.LoginProvider)).ToList();
             ViewData["ShowRemoveButton"] = user.PasswordHash != null || userLogins.Count > 1;
             return View(new ManageLoginsViewModel
             {
